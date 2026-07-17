232cc710875683a87c20da98b3af2e1a99e0a306
NANDIKA SOI <69952137+nandikasoi@users.noreply.github.com>
2026-07-17 23:33:23 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+
 
 export default defineConfig({
   site: 'https://vibe-portfolio-site.pages.dev',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind() ],
 });
